fa2b0e82da3c16f30733ca4a70a45293ac3cfcdc
Chase Starr <chasestarr@gmail.com>
2016-11-01 15:01:27 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const vision = require('node-cloud-vision-api');
+const express = require("express");
+const multer  = require('multer');
+const crypto = require('crypto');
+const path = require('path');
+
+vision.init({auth: 'AIzaSyB34hbutVzDgi6foXkDZ5kIjsQtu1zAthU'});
+
+const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, 'username' + path.extname(file.originalname));
+  },
+});
+const upload = multer({ storage : storage}).single('userPhoto');
+
+app.get('/',function(req,res){
+      res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/api/photo',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        let username = 'username';
+        let image = require('path').resolve(__dirname, 'uploads/' + username + '.JPG');
+        console.log('about to ocr');
+
+
+        // ==== Google vision api ====
+        const req = new vision.Request({
+          image: new vision.Image(image),
+          features: [
+            new vision.Feature('TEXT_DETECTION', 10)
+          ]
+        });
+
+        vision.annotate(req).then((res) => {
+          // handling response
+          console.log(JSON.stringify(res.responses[0].textAnnotations[0].description, null, 1));
+        }, (e) => {
+          console.log('Error: ', e)
+        });
+    });
+});
+
+app.listen(3000,function(){
+    console.log("Working on port 3000");
+});
